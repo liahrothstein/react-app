bdbb38bae980628a3e648cb3503f220487f5914e
Dmitry Novikov <liahrothstein@gmail.com>
2022-07-02 01:47:27 +0300

    Recipes was added

---
+++ project/src/components/GridContainer1/Recipes/Recipe2.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Recipe2 = function (){
+    return(
+        <div className="recipe2">
+            <div className="image"></div>
+            <div className="recipeName">Giant Ice Cream Sandwich</div>
+            <div className="more">
+                <div className="time">🕘20mins</div>
+                <div className="numbers">🖅4👀356</div>
+            </div>
+        </div>
+    )
+}
+
+export default Recipe2;
